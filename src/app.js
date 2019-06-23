f92ed5f29a40e3dba0b20f4bc1cf02dfe0669a5d
Joshua <olajidejoshua4real@gmail.com>
2019-06-23 21:17:58 +0100

    Get method created on the app variable

--- src/app.js
+++ src/app.js
@@ -4,6 +4,15 @@ let express = require('express');
 
 let app = express();
 
-// Setting up the development server using the listen method
+ /**
+  * Setting up the development server using the listen method
+  * 
+  * Create the get method on the app variable
+  */
+ // The slash parameter is sometimes called the location parameter
+ app.get('/', function(request, response){
+     response.send("I love Treehouse!");
+ })
+
 
 app.listen(3000);
