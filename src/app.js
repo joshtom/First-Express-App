43f8676c96bef032a8e28375755d88d1d8f6fb4a
Joshua <olajidejoshua4real@gmail.com>
2019-06-24 16:13:46 +0100

    Router changed to display dynamic information

--- src/app.js
+++ src/app.js
@@ -9,16 +9,21 @@ let app = express();
   * Setting up the development server using the listen method
   * 
   * Create the get method on the app variable
+  * 
+  * Adding a parameter to the route
   */
  // The slash parameter is sometimes called the location parameter
  app.get('/', function(req, res){
-     res.send("<h1>I almost thought i did not love Treehouse!</h1>");
+     res.send("<h1>I love Myself and I, I hope this will work now!</h1>");
  })
 
- app.get('/blog', function(req, res){
-    res.send(posts);
+ app.get('/blog/:title', function(req, res){
+     debugger;
+     let title = req.params.title;
+     let post = posts[title];
+    res.send(post);
  });
 // Taking a callback function as a parameter
-app.listen(3000, function(){
+app.listen(5000, function(){
     console.log("FrontEnd server is running on port 3000");
 });
